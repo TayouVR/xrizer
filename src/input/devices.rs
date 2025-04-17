3795daa6f42f126c889d4a829bb65073815efbd4
Sarah <xxklintan123xx@gmail.com>
2025-04-17 18:39:32 +0200

    devices: cleanup

--- src/input/devices.rs
+++ src/input/devices.rs
@@ -13,7 +13,6 @@ pub struct TrackedDeviceList {
     devices: Vec<XrTrackedDevice>,
 }
 
-#[allow(dead_code)]
 impl TrackedDeviceList {
     pub fn new(xr_instance: &xr::Instance) -> Self {
         Self {
@@ -33,30 +32,6 @@ impl TrackedDeviceList {
 
     pub fn get_device(&self, device_index: vr::TrackedDeviceIndex_t) -> Option<&XrTrackedDevice> {
         self.devices.get(device_index as usize)
-    }
-
-    pub fn get_device_mut(
-        &mut self,
-        device_index: vr::TrackedDeviceIndex_t,
-    ) -> Option<&mut XrTrackedDevice> {
-        self.devices.get_mut(device_index as usize)
-    }
-
-    /// This function is only intended to be used for the HMD and controllers. For other devices, it'll return the first match.
-    pub fn get_device_by_type(&self, device_type: TrackedDeviceType) -> Option<&XrTrackedDevice> {
-        self.devices
-            .iter()
-            .find(|device| device.get_type() == device_type)
-    }
-
-    /// This function is only intended to be used for the HMD and controllers. For other devices, it'll return the first match.
-    pub fn get_device_by_type_mut(
-        &mut self,
-        device_type: TrackedDeviceType,
-    ) -> Option<&mut XrTrackedDevice> {
-        self.devices
-            .iter_mut()
-            .find(|device| device.get_type() == device_type)
     }
 
     pub fn get_hmd(&self) -> &XrTrackedDevice {
